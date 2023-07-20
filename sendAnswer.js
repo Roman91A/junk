d5dacc2ddd2efc352ba17a81ac89163f2398e756
Roman91A <106251537+Roman91A@users.noreply.github.com>
2023-07-20 19:47:39 +0300

    Update sendAnswer.js

--- sendAnswer.js
+++ sendAnswer.js
@@ -1,52 +1,37 @@
-/*
-** функция принимает 1 или 2 аргумента
-      Если передан один аргумент 
-        проверяем что это объект формата {'text': 'текст'} или {'text': 'текст', 'hardChips'; 'чипс'}
-        Если в объекте найдены text и hardChips или text, то пушим, иначе возвращем ошибку 
+import * as _ from 'underscore';
+import yaml from 'js-yaml';
+import fs from 'fs';
+import { appendFile } from 'fs/promises';
 
-      Если передано 2 аргумента, то предыдущая логика 
+function sendAnswer(pathToObject) {
+    function validationObjectResponse(response) {
+        return !(
+            (Object.keys(response).length === 2 && (_.isUndefined(response.text) || _.isUndefined(response.hardChips))) ||
+            (Object.keys(response).length === 1 && _.isUndefined(response.text))
+        );
+    }
 
-*/
-
-
-function sendAnswer() {
-    if (arguments.length === 1 && _.isObject(arguments[0])) {
-        if (Object.keys(arguments[0]).length == 2) {
-            if (!_.isUndefined(arguments[0].text) && !_.isUndefined(showChip([arguments[0].hardChips]))) {
-                $reactions.answer(arguments[0].text)
-                showChip([arguments[0].hardChips])
-            } else { 
-                transferFromIntegration("sendAnswer(): text || hardChips - Undefined ");
-            }
-        } else {
-            if (!_.isUndefined(arguments[0].text)) {
-                $reactions.answer(arguments[0].text)
-            } else { 
-                transferFromIntegration("sendAnswer(): text - Undefined ");
-            }
+    function pushResponse(element) {
+        if (!_.isUndefined(element.text)) {
+            console.log('отправляем текст ' + element.text);
         }
-        if (arguments.length === 2 && _.isObject(arguments[0]) && _.isArray(arguments[1])) {
-            var file = file;
-            if (_.isArray(responsePath)) {
-                _.map(responsePath, function (path) {
-                    var path = file[path.split('.')[0]][path.split('.')[1]]
-                    _.map(Object.keys(path), function (textHardChips) {
-                        if (textHardChips == 'text') {
-                            if (path.text.length == 1) {
-                                $reactions.answer(path.text[0])
-                            }
-                            else {
-                                _.map(path.text, function (text) {
-                                    $reactions.answer(text);
-                                })
-                            }
-                        }
-                        if (textHardChips == 'hardChips') {
-                            showChip(path.hardChips);
-                        }
-                    })
-                })
-            }
+        if (!_.isUndefined(element.hardChips)) {
+            console.log('отправляем чипс ' + element.hardChips);
         }
     }
+
+    if (!Array.isArray(pathToObject)) {
+        if (validationObjectResponse(pathToObject)) {
+            pushResponse(pathToObject);
+        } else {
+            return false;
+        }
+    } else {
+        if (_.every(pathToObject, (obj) => obj && (!_.isUndefined(obj.text) || !_.isUndefined(obj.hardChips)))) {
+            _.each(pathToObject, pushResponse);
+        } else {
+            return false;
+        }
+    }
+    return true;
 }
