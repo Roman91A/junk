381abba2b3bd5a711927511123a6add3a55c932e
Roman91A <106251537+Roman91A@users.noreply.github.com>
2023-07-20 19:57:05 +0300

    Update sendAnswer.js

--- sendAnswer.js
+++ sendAnswer.js
@@ -1,16 +1,19 @@
-import * as _ from 'underscore';
-import yaml from 'js-yaml';
-import fs from 'fs';
-import { appendFile } from 'fs/promises';
+/** Пушим ответы из yaml
+* @param {pathToObject}  pathToObject - Объект или массив объектов с ответоами и чипсами
+* Если аргумент валиден, то функция пушит ответы и возвращает true
+* Если один из объектов невалиден, то ответы не пушаться, функция возвращается folse 
+*/
+
 
 function sendAnswer(pathToObject) {
-    function validationObjectResponse(response) {
+    // Валидация объета на наличие text и hardChips
+    function validationObjectResponse(response) { 
         return !(
             (Object.keys(response).length === 2 && (_.isUndefined(response.text) || _.isUndefined(response.hardChips))) ||
             (Object.keys(response).length === 1 && _.isUndefined(response.text))
         );
     }
-
+     // Пушим text и hardChips
     function pushResponse(element) {
         if (!_.isUndefined(element.text)) {
             console.log('отправляем текст ' + element.text);
@@ -19,7 +22,7 @@ function sendAnswer(pathToObject) {
             console.log('отправляем чипс ' + element.hardChips);
         }
     }
-
+    // Пушим text и hardChips если sendAnswer был вызван с одним объектом, если что-то пошло не так, возвращаем false
     if (!Array.isArray(pathToObject)) {
         if (validationObjectResponse(pathToObject)) {
             pushResponse(pathToObject);
@@ -27,6 +30,7 @@ function sendAnswer(pathToObject) {
             return false;
         }
     } else {
+        // Пушим text и hardChips если sendAnswer был вызван с массивом объектом, если что-то пошло не так, возвращаем false
         if (_.every(pathToObject, (obj) => obj && (!_.isUndefined(obj.text) || !_.isUndefined(obj.hardChips)))) {
             _.each(pathToObject, pushResponse);
         } else {
